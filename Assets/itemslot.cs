c86a2bbf2035d82b62e47b2d19046608bd559662
unhappybreadroll <samdoherty007@gmail.com>
2020-11-24 10:50:01 +0000

    inital commit

---
+++ Assets/itemslot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class itemslot : MonoBehaviour, IDropHandler {
+
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null) {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            //this makes the items snapp into place
+        }
+    }
+
+
+
+
+    }
